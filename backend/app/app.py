0c0dc0e2b87a0d47cb80f8f33182e9f4b1383d85
Rajeshbabu21 <rajeshgit2004@gmail.com>
2026-03-09 21:06:51 +0530

    added url

--- backend/app/app.py
+++ backend/app/app.py
@@ -21,7 +21,9 @@ app.add_middleware(
 
     allow_origins=[
     "http://localhost:5173", 
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
+    "https://resumemax.vercel.app"
+    
     ],
     
     allow_credentials=True,
